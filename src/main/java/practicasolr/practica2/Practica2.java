1cf5e3f5ed5a349afdf1e5d5720a144d1a95e7cb
PabloGomez03 <jbarea155@gmail.com>
2025-10-24 16:56:11 +0200

    Queries completadas

--- src/main/java/practicasolr/practica2/Practica2.java
+++ src/main/java/practicasolr/practica2/Practica2.java
@@ -1,24 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 package practicasolr.practica2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.util.ClientUtils;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 
-/**
- *
- * @author apolo
- */
 public class Practica2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SolrServerException, IOException {
 
         String file = "..\\corpus\\MED.QRY";
         boolean end = false, start = true, done = false;
@@ -27,81 +26,89 @@ public class Practica2 {
 
             String line = "";
             String id = "";
+            ArrayList<String> words = new ArrayList();
+            int it = 0;
 
             final SolrClient client = new HttpSolrClient.Builder("http://localhost:8983/solr").build();
 
             do {
-                ArrayList<String> words = new ArrayList();
-                
+
+                words.clear();
+
                 if (start) {
                     line = br.readLine();
                 }
 
-                if (line.length() == 0) {
+                if (line == null || line.trim().isEmpty()) {
 
                     end = true;
 
                 } else {
 
-                    id = line.trim().split("I")[1];
+                    id = line.substring(2).trim();
                     br.readLine();
 
                     line = br.readLine();
                     done = false;
-                    while (line != null && line.charAt(1) != 'I') {
+                    while (line != null && !line.startsWith(".I")) {
 
-                        String[] ar = line.split("\\s+");
+                        String trimmedLine = line.trim();
 
-                        for (int i = 0; i < ar.length; i++) {
-                            if (words.size() < 6) {
-                                words.add(ar[i]);
+                        if (!trimmedLine.isEmpty()) {
+                            String[] ar = trimmedLine.split("\\s+");
+
+                            for (int i = 0; i < ar.length; i++) {
+                                if (words.size() < 5) {
+                                    words.add(ar[i]);
+                                }
                             }
-
                         }
 
                         line = br.readLine();
                     }
-                    
 
                     if (line == null) {
                         end = true;
                     }
 
-                }
-                
-                
-                System.out.println("\nPalabras de id ->"+id+"\n-----------------------");
-                for(String w:words){
-                    System.out.println("| "+w);
-                    
-                    SolrQuery q = new SolrQuery();
-                    
-                    q.setQuery("");
-                    
-                    
-                }
-                
-                
-                //System.out.println(id+" -------> "+text+"\n");
-                /*SolrInputDocument doc = new SolrInputDocument();
-                doc.addField("identifier", id);
-                doc.addField("text", text);
-                UpdateResponse updateResponse = client.add("Practica1", doc);
-                client.commit("Practica1");
+                    System.out.println("\nPalabras de id ->" + id + "\n-----------------------");
+                    for (String w : words) {
+                        System.out.println("| " + w);
+                    }
 
-                
+                    String searchTerms = String.join(" ", words);
+                    String escapedSearchTerms = ClientUtils.escapeQueryChars(searchTerms);
+                    SolrQuery q = new SolrQuery();
+
+                    q.setQuery(escapedSearchTerms);
+
+                    q.set("df", "text");
+                    q.setRows(100);
+
+                    QueryResponse response = client.query("Practica1", q);
+
+                    SolrDocumentList list = response.getResults();
+
+                    try (BufferedWriter bw = new BufferedWriter(new FileWriter("Queries\\queryRes" + (it + 1)))) {
+                        for (SolrDocument doc : list) {
+                            bw.write("Document " + doc.getFieldValue("identifier") + "\n------------------\n");
+                            bw.write(doc.getFieldValue("text") + "\n\n");
+                        }
+                    }
+
+                }
+
                 id = "";
-                text = "";*/
-                id = "";
-                
+                it++;
                 start = false;
-                
+
             } while (!end);
+            client.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
+            e.printStackTrace();
         }
 
     }
 
 }
-
